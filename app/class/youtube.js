def245730b43a6b9c3eaaba1d262b3dd44949ef1
Manu16 <116045677+ManuD33P@users.noreply.github.com>
2025-06-20 16:44:23 -0300

    api key change

--- app/class/youtube.js
+++ app/class/youtube.js
@@ -1,5 +1,7 @@
+
+
 async function fetchYouTubeVideos(query) {
-    const API_KEY = 'AIzaSyDRY715OMrAa5cdKI2lPNFuliMY6mazqSg'; // Reemplaza con tu clave de API de YouTube
+    const API_KEY = 'AIzaSyAHqpHnF0ltaDodbnyQM8Up7ibuRVUyn-U'; // Reemplaza con tu clave de API de YouTube
     const url = `https://www.googleapis.com/youtube/v3/search?part=snippet&type=video&maxResults=10&q=${(query)}&key=${API_KEY}`;
 
     try {
