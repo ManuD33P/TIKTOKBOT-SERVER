9701f6cbfa3e4c9a514a23bd7b9e829b5a8199b0
Emanuel Dominguez <116045677+ManuD33P@users.noreply.github.com>
2025-11-06 23:04:51 -0300

    Refactor getBinary to remove proxy handling
    Removed proxy fetching and error handling from getBinary method.

--- app/class/render-audio.js
+++ app/class/render-audio.js
@@ -26,30 +26,24 @@ class TSSAudio {
         try {
             if (!this.text) throw new Error('Text is undefined');
 
-            const proxies = await this.fetchProxies();
-            if (proxies.length === 0) throw new Error("No hay proxies disponibles");
-
             let audioBase64 = null;
 
-            for (const proxy of proxies) {
-                const [host, port] = proxy.split(":");
+        
 
-                try {
+              
                     const audiobs64 = await googleTSS.getAudioBase64(this.text, {
                         lang: "es",
                         slow: false,
                     });
 
+                    console.log('valor de audio: ',audiobs64)
                     if (!audiobs64) throw new Error("Error al obtener audio.");
 
                     this.source = `data:audio/mp3;base64,${audiobs64}`;
                     audioBase64 = audiobs64;
                     console.log("¡Audio generado exitosamente!");
-                    break;
-                } catch (error) {
-                    console.log(`Error con el proxy ${proxy}, probando otro...`);
-                }
-            }
+
+            
 
             if (!audioBase64) {
                 throw new Error("No se pudo generar el audio con ningún proxy.");
